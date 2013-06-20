8660098c2ef1e8180e23f4cd98d93c9ba3c98455
Daniel Miedema <danielmiedema@me.com>
2013-06-19 23:07:15 -0700

    Complete redefine of interface. Go me.

--- aBGHC/DMGitHubClient.h
+++ aBGHC/DMGitHubClient.h
@@ -9,7 +9,8 @@
 #import <Foundation/Foundation.h>
 #import <AFNetworking/AFNetworking.h>
 
-typedef void (^JSONResponseBlock)(NSArray *json);
+typedef void (^JSONResponseBlock)(id json);
+typedef void (^ErrorResponseBlock)(NSDictionary *error);
 
 @interface DMGitHubClient : NSObject
 
@@ -25,19 +26,27 @@ typedef enum {
   FILES,
 } repositoryContentType;
 
+typedef enum {
+  REPOSITORIES,
+  USERS
+} searchType;
+
 + (DMGitHubClient *)sharedInstance;
 + (NSArray *)homeScreenOptions;
 + (NSArray *)searchScopeOptions;
 
-- (NSArray *)loadRepositoriesWithType:repositoryTypesToLoad andOptions:(NSDictionary *)options;
-- (NSArray *)loadInformationForRepository:repositoryContentType withOptions:(NSDictionary *)options;
-
-- (NSDictionary *)loadFileWithInformation:(NSDictionary *)fileInformation;
-
-- (void)getNotificationsForUserWithCallback:(JSONResponseBlock)callback;
-
-- (NSArray *)searchForRepositoriesWithCriteria:(NSDictionary *)criteria;
-- (NSArray *)searchForUsersWithCriteria:(NSDictionary *)criteria;
-
-- (BOOL)createCommitWithInformation:(NSDictionary *)commitInformation;
+// Load repository content
+- (void)loadRepositoryInformation:(repositoryContentType)infoType forRepo:(NSDictionary *)repository withSuccess:(JSONResponseBlock)success andError:(ErrorResponseBlock)error;
+// Load repository details
+- (void)loadRepositoriesWithOptions:(repositoryTypesToLoad)repoType onSuccess:(JSONResponseBlock)success andError:(ErrorResponseBlock)failure;
+// Load a particular file
+- (void)loadFileWithInformation:(NSDictionary *)dictionary withSuccess:(JSONResponseBlock)success andError:(ErrorResponseBlock)failure;
+// Get notifications
+- (void)getNotificationsForUserWithSuccess:(JSONResponseBlock)success andError:(ErrorResponseBlock)failure;
+// Search
+- (void)searchWithType:(searchType)type andCriteria:(NSDictionary *)criteria withSuccess:(JSONResponseBlock)success andError:(ErrorResponseBlock)error;
+// Create a file
+- (void)createNewFileWithInformation:(NSDictionary *)information withSuccess:(JSONResponseBlock)sucess andError:(ErrorResponseBlock)error;
+// Create a Commit
+- (void)createCommitWithInformation:(NSDictionary *)commitInformation withSuccess:(JSONResponseBlock)success andError:(ErrorResponseBlock)error;
 @end
