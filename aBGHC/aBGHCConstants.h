9279d19df65a32eb19b592ac58a842054dc5a169
Daniel Miedema <danielmiedema@me.com>
2013-06-24 02:09:02 -0700

    Removed #defines and went to static NSStrings as well as added some frameworks AFNetworking wanted in the prefix file.

--- aBGHC/aBGHCConstants.h
+++ aBGHC/aBGHCConstants.h
@@ -10,24 +10,27 @@
 
 #pragma mark GitHub Constants
 
-#define aBGHC_GitHubClientID            @"8881762e516271c9af67"
-#define aBGHC_GitHubClientSecret        @"a850b277689c5cc93e3dbbcbe12e96011f972ecf"
-#define aBGHC_GitHubScope               @"user,notifications,repo,gist,delete_repo"
+#pragma mark - Configuration/Account
+static NSString *const aBGHC_GitHubClientID             = @"8881762e516271c9af67";
+static NSString *const aBGHC_GitHubClientSecret         = @"a850b277689c5cc93e3dbbcbe12e96011f972ecf";
+static NSString *const aBGHC_GitHubScope                = @"user,notifications,repo,gist,delete_repo";
 
-#define aBGHC_GitHubApiUrl              @"https://api.github.com"
+#pragma mark - Model
+static NSString *const aBGHC_GitHubApiUrl               = @"https://api.github.com";
+static NSString *const aBGHC_DefaultHTTPType            = @"application/json";
+static NSString *const aBGHC_DefaultHTTPMethod          = @"GET";
 
-#define aBGHC_DefaultHTTPType    @"application/json"
-#define aBGHC_DefaultHTTPMethod  @"GET"
+#pragma mark - NSUserDefaults
+static NSString *const aBGHC_UUID                       = @"aBGHC_UUID";
+static NSString *const aBGHC_AccessToken                = @"access_token";
+static NSString *const aBGHC_TokenType                  = @"token_type";
+static NSString *const aBGHC_Username                   = @"username";
 
-#define aBGHC_UUID              @"aBGHC_UUID"
 
-#define aBGHC_AccessToken  @"access_token"
-#define aBGHC_TokenType    @"token_type"
-#define aBGHC_Username     @"username"
+#pragma mark - NSNotificationCenter Notifications
+static NSString *const aBGHC_CommitCancelledNotification      = @"aBGHC-Commit-Cancelled-Notification";
+static NSString *const aBGHC_CommitMessagePostedNotification  = @"aBGHC-Commit-Posted-Notification";
 
-#define aBGHC_CommitCancelledNotification       @"aBGHC-Commit-Cancelled-Notification"
-#define aBGHC_CommitMessagePostedNotification   @"aBGHC-Commit-Posted-Notification"
+static NSString *const aBGHC_Model_NotificationsRecieved      = @"aBGHC-Model-NotifcationsForUserRecieved";
 
-#define aBGHC_Model_NotificationsRecieved       @"aBGHC-Model-NotifcationsForUserRecieved"
-
-#define aBGHC_TintColor [UIColor greenColor];
+//static NSString *const aBGHC_TintColor [UIColor greenColor];
