630a77c5af5f5bb4eefee370ed39904a0654c62c
Daniel Miedema <danielmiedema@me.com>
2013-06-26 01:32:42 -0700

    Repository TableViewController added.

---
+++ aBGHC/DMRepositoriesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DMRepositoriesTableViewController.h
+//  aBGHC
+//
+//  Created by Daniel Miedema on 6/25/13.
+//  Copyright (c) 2013 Daniel Miedema. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DMRepositoriesTableViewController : UITableViewController
+
+@end
