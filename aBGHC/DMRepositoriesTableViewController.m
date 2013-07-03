11d9462e98d7d7610d89aa5ac61c6d381ebaf3d1
Daniel Miedema <danielmiedema@me.com>
2013-07-02 22:43:24 -0700

    Removed Search and added segment control.

--- aBGHC/DMRepositoriesTableViewController.m
+++ aBGHC/DMRepositoriesTableViewController.m
@@ -12,45 +12,54 @@
 @interface DMRepositoriesTableViewController ()
 //@property (nonatomic) NSMutableArray *searchResults;
 //@property BOOL scrollViewIsAtTop;
+
+@property (nonatomic, strong) UISegmentedControl *segmentControl;
+
+- (void)updateTable;
+- (void)segmentControlChanged:(UISegmentedControl *)sender;
+
 @end
 
 @implementation DMRepositoriesTableViewController
 
-- (id)initWithStyle:(UITableViewStyle)style
-{
-    self = [super initWithStyle:style];
-    if (self) {
-        // Custom initialization
-        
-    }
-//    self.navigationController.searchDisplayController 
-    return self;
-}
-
-
+#pragma mark - Life Cycle
 
 - (instancetype)init {
     self = [super init];
     if (self) {
-//        [NSNotificationCenter defaultCenter] addObserver:self selector:@selector(scopeChanged:) name: object:<#(id)#>
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateTable) name:aBGHC_NewAccountCreatedNotification object:nil];
     }
     return self;
 }
 
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+//    [[DMGitHubClient sharedInstance] cancelNetworkRequests];
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    // Offset/hide search bar on view load.
+//    self.tableView.contentOffset = CGPointMake(0.0, self.searchDisplayController.searchBar.frame.size.height);
+    
+    _segmentControl = [[UISegmentedControl alloc] initWithItems:[DMGitHubClient searchScopeOptions]];
+    [_segmentControl addTarget:self action:@selector(segmentControlChanged:) forControlEvents:UIControlEventValueChanged];
+    _segmentControl.frame = self.tableView.tableHeaderView.frame;
+    
+    _segmentControl.selectedSegmentIndex = 0;
+    
     [[DMGitHubClient sharedInstance] loadRepositoriesWithOptions:MINE onSuccess:^(id JSON) {
-        NSLog(@"JSON: %@", JSON);
+//        NSLog(@"JSON: %@", JSON);
         _repos = JSON;
-        [self.tableView reloadData];
+        [self updateTable];
     } andError:^(NSDictionary *error) {
         NSLog(@"ERRR");
     }];
-//    _searchResults = [NSMutableArray new];
     
-    self.searchDisplayController.searchBar.scopeButtonTitles = [DMGitHubClient searchScopeOptions];
+    // Set up search scope
+//    self.searchDisplayController.searchBar.scopeButtonTitles = [DMGitHubClient searchScopeOptions];
+//    NSLog(@"search results tableView: %@", self.searchDisplayController.searchResultsTableView);
     
 //    self.searchDisplayController.displaysSearchBarInNavigationBar = YES;
     // Uncomment the following line to preserve selection between presentations.
@@ -58,6 +67,11 @@
  
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [[DMGitHubClient sharedInstance] cancelNetworkRequests];
+    [super viewWillDisappear:animated];
 }
 
 - (void)didReceiveMemoryWarning
@@ -88,6 +102,8 @@
     static NSString *CellIdentifier = @"Cell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
     
+    NSLog(@"Section : %i", (int)indexPath.section);
+    
     // Configure the cell...
     
 //    [cell addSubview:[DMRepositoryTableViewCell createTableViewCellWithBounds:cell.bounds andWithDictionary:[_repos objectAtIndex:indexPath.row]]];
@@ -97,9 +113,9 @@
 //    if (tableView == self.searchDisplayController.searchResultsTableView) {
 //        current = [_searchResults objectAtIndex:indexPath.row];
 //    } else {
-        current = [_repos objectAtIndex:indexPath.row];
+//        current = [_repos objectAtIndex:indexPath.row];
 //    }
-    
+    current = _repos[indexPath.row];
     cell.textLabel.text = [current objectForKey:@"name"];
     cell.detailTextLabel.text = [[current objectForKey:@"owner"] objectForKey:@"login"];
     
@@ -111,44 +127,98 @@
     return 64;
 }
 
+//- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+//    if (section == 0) {
+//        return _segmentControl.frame.size.height;
+//    }
+//    return 0;
+//}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    if (section == 0) {
+        return _segmentControl;
+    }
+    return nil;
+}
 
 #pragma mark - UISearchDisplayController Delegate Methods
 
-- (BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString
-{    
-    // Return YES to cause the search result table view to be reloaded.
-    return NO;
+//- (BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString
+//{    
+//    // Return YES to cause the search result table view to be reloaded.
+//    return NO;
+//}
+//
+//
+//- (BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchScope:(NSInteger)searchOption
+//{
+//    NSLog(@"Search Scope Changed");
+//    NSLog(@"%@", [[controller.searchBar.scopeButtonTitles objectAtIndex:searchOption] uppercaseString]);
+//    
+//    NSString *selectedItem = [controller.searchBar.scopeButtonTitles objectAtIndex:searchOption];
+//    
+//    NSDictionary *mappings = @{
+//                               @"Mine" : [[NSNumber alloc] initWithInt:MINE],
+//                               @"Starred" : [[NSNumber alloc] initWithInt:STARRED],
+//                               @"Watching" : [[NSNumber alloc] initWithInt:WATCHED]
+//                               };
+//    
+//    if (![selectedItem isEqualToString:@"All"]) {
+//        [[DMGitHubClient sharedInstance] loadRepositoriesWithOptions:(int)[mappings objectForKey:selectedItem] onSuccess:^(id JSON) {
+//            NSLog(@"Selected JSON: %@", JSON);
+//                if (self.isViewLoaded) {
+//                    _repos = JSON;
+//                    [self.tableView reloadData];
+//                }
+//        } andError:^(NSDictionary *error) {
+//            NSLog(@"Selected ERRR: %@", error);
+//        }];
+//    }
+//        // Return YES to cause the search result table view to be reloaded.
+////    return YES;
+//    return NO;
+//}
+
+#pragma mark - Private Methods
+- (void)updateTable {
+    [self.tableView reloadData];
+//    self.tableView.contentOffset = CGPointMake(0.0, self.searchDisplayController.searchBar.frame.size.height);
 }
 
-
-- (BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchScope:(NSInteger)searchOption
-{
-    NSLog(@"Search Scope Changed");
-    NSLog(@"%@", [[controller.searchBar.scopeButtonTitles objectAtIndex:searchOption] uppercaseString]);
-    
-    NSString *selectedItem = [controller.searchBar.scopeButtonTitles objectAtIndex:searchOption];
-    
-    NSDictionary *mappings = @{
-                               @"Mine" : [[NSNumber alloc] initWithInt:MINE],
-                               @"Starred" : [[NSNumber alloc] initWithInt:STARRED],
-                               @"Watching" : [[NSNumber alloc] initWithInt:WATCHED]
-                               };
-    
-    if (![selectedItem isEqualToString:@"All"]) {
-        [[DMGitHubClient sharedInstance] loadRepositoriesWithOptions:(int)[mappings objectForKey:selectedItem] onSuccess:^(id JSON) {
-            NSLog(@"Selected JSON: %@", JSON);
-            _repos = JSON;
-            [self.tableView reloadData];
-        } andError:^(NSDictionary *error) {
-            NSLog(@"Selected ERRR: %@", error);
-        }];
+- (void)segmentControlChanged:(UISegmentedControl *)sender {
+    int selectedIndex = [sender selectedSegmentIndex];
+    switch (selectedIndex) {
+        case 0: { // Mine
+            [[DMGitHubClient sharedInstance] loadRepositoriesWithOptions:MINE onSuccess:^(id JSON) {
+                _repos = JSON;
+                [self updateTable];
+            } andError:^(NSDictionary *error) {
+                NSLog(@"Error loading mine: %@", error);
+            }];
+            break;
+        }
+        case 1: { // Starred
+            [[DMGitHubClient sharedInstance] loadRepositoriesWithOptions:STARRED onSuccess:^(id JSON) {
+                _repos = JSON;
+                [self updateTable];
+            } andError:^(NSDictionary *error) {
+                NSLog(@"Error loading starred: %@", error);
+            }];
+            break;
+        }
+        case 2: { // Watching
+            [[DMGitHubClient sharedInstance] loadRepositoriesWithOptions:WATCHED onSuccess:^(id JSON) {
+                _repos = JSON;
+                [self updateTable];
+            } andError:^(NSDictionary *error) {
+                NSLog(@"Error loading watching: %@", error);
+            }];
+            break;
+        }
+        default:
+            break;
     }
-        // Return YES to cause the search result table view to be reloaded.
-    return YES;
 }
-
-
-
 
 /********************
  
